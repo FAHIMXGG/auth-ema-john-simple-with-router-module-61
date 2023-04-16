88b88b9f2add56cd3abed3d9555f01c89ba6efb6
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-04-17 00:17:37 +0600

    sign up login

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Header.css';
 import logo from '../../images/Logo.svg';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProvider';
 
 const Header = () => {
+
+    const {user, logOut} = useContext(AuthContext)
+
+    const handleLogout = () =>{
+        
+    }
     return (
         <nav className='header'>
             <img src={logo} alt="" />
@@ -12,6 +19,10 @@ const Header = () => {
                 <Link to="/orders">Orders</Link>
                 <Link to="/inventory">Inventory</Link>
                 <Link to="/login">Login</Link>
+                <Link to="/signup">Sign Up</Link>
+                {
+                    user && <span>welcome,{user.email} <button onClick={handleLogout}>Sign Out</button></span>
+                }
             </div>
         </nav>
     );
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,46 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
+import './Login.css'
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProvider';
 const Login = () => {
+
+    const {signIn} = useContext(AuthContext);
+
+    const handleLogin = e =>{
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+
+        signIn(email, password)
+        .then(result =>{
+            const loggedUser = result.user;
+            console.log(loggedUser);
+            form.reset();
+        })
+        .catch(error =>{
+            console.log(error);
+        })
+    }
+
+
     return (
-        <div>
-            <h2>Login coming sooon</h2>
+        <div className='form-container'>
+            <h2 className='form-title '>Login</h2>
+            <form onSubmit={handleLogin}>
+                <div className='form-control'>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="" required />
+                </div>
+                <div className='form-control'>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="" required />
+                </div>
+                <input className="btn-submit" type="submit" value="login" />
+
+            </form>
+            <p><small>New? <Link to="/login">Login</Link></small></p> 
         </div>
     );
 };
